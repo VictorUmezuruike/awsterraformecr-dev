53c27042cdc5f7778ac514aabf30f366b558cd79
VictorUmezuruike <victor.umezuruike@toyingroupsystems.com>
2023-01-18 13:19:28 +0100

    updated terraform config

--- infrastructure/modules/tf-state/tf-state.tf
+++ infrastructure/modules/tf-state/tf-state.tf
@@ -22,21 +22,21 @@ resource "aws_ecs_cluster" "production" {
 resource "aws_ecs_service" "mongo" {
   name            = "mongodb"
   cluster         = aws_ecs_cluster.production.id
-  task_definition = aws_ecs_task_definition.production.arn
+#   task_definition = aws_ecs_task_definition.production.arn
   desired_count   = 3
-  iam_role        = aws_iam_role.production.arn
-  depends_on      = [aws_iam_role_policy.production]
+#   iam_role        = aws_iam_role.production.arn
+#   depends_on      = [aws_iam_role_policy.production]
 
   ordered_placement_strategy {
     type  = "binpack"
     field = "cpu"
   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.production.arn
-    container_name   = "mongo"
-    container_port   = 8080
-  }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.production.arn
+#     container_name   = "mongo"
+#     container_port   = 8080
+#   }
 
   placement_constraints {
     type       = "memberOf"
