5e99359ba2cb4ad4a02a48f926a9e79ccc8be927
VictorUmezuruike <victor.umezuruike@toyingroupsystems.com>
2023-01-18 13:47:22 +0100

    updated terraform config

--- infrastructure/modules/tf-state/tf-state.tf
+++ infrastructure/modules/tf-state/tf-state.tf
@@ -1,6 +1,6 @@
 #ecr repository
 resource "aws_ecr_repository" "production" {
-  name                 = "prod-env"
+  name                 = "prod-env2"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
