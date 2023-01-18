28eb3518f8452abf622b0c4adb79706c50e0d3fd
VictorUmezuruike <victor.umezuruike@toyingroupsystems.com>
2023-01-18 13:45:36 +0100

    updated terraform config

--- infrastructure/modules/tf-state/tf-state.tf
+++ infrastructure/modules/tf-state/tf-state.tf
@@ -18,28 +18,28 @@ resource "aws_ecs_cluster" "production" {
   }
 }
 
-#ecs service
-resource "aws_ecs_service" "mongo" {
-  name            = "mongodb"
-  cluster         = aws_ecs_cluster.production.id
-#   task_definition = aws_ecs_task_definition.production.arn
-  desired_count   = 3
-#   iam_role        = aws_iam_role.production.arn
-#   depends_on      = [aws_iam_role_policy.production]
+# #ecs service
+# resource "aws_ecs_service" "mongo" {
+#   name            = "mongodb"
+#   cluster         = aws_ecs_cluster.production.id
+# #   task_definition = aws_ecs_task_definition.production.arn
+#   desired_count   = 3
+# #   iam_role        = aws_iam_role.production.arn
+# #   depends_on      = [aws_iam_role_policy.production]
 
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
-  }
-
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.production.arn
-#     container_name   = "mongo"
-#     container_port   = 8080
+#   ordered_placement_strategy {
+#     type  = "binpack"
+#     field = "cpu"
 #   }
 
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
-  }
-}
+# #   load_balancer {
+# #     target_group_arn = aws_lb_target_group.production.arn
+# #     container_name   = "mongo"
+# #     container_port   = 8080
+# #   }
+
+#   placement_constraints {
+#     type       = "memberOf"
+#     expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
+#   }
+# }
