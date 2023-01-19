b1d7eb47c7e05ef903056502b6cacdfe3bdbe9b5
VictorUmezuruike <victor.umezuruike@toyingroupsystems.com>
2023-01-19 11:10:43 +0100

    push docker image to ecr

--- infrastructure/modules/tf-state/tf-state.tf
+++ infrastructure/modules/tf-state/tf-state.tf
@@ -1,6 +1,6 @@
 #ecr repository
 resource "aws_ecr_repository" "production" {
-  name                 = "prod-env2"
+  name                 = "prod-env"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
