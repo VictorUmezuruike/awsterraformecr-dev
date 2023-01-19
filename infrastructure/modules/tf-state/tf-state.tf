b566f36a7987075b67c2060a3c94d1dad8136c81
VictorUmezuruike <victor.umezuruike@toyingroupsystems.com>
2023-01-19 13:41:20 +0100

    added docker push command

--- infrastructure/modules/tf-state/tf-state.tf
+++ infrastructure/modules/tf-state/tf-state.tf
@@ -1,6 +1,6 @@
 #ecr repository
 resource "aws_ecr_repository" "production" {
-  name                 = "test-env2"
+  name                 = "poc-env"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
