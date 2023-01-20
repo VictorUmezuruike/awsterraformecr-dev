05ab0415f70990673a7e09a0ee757337cfad4ec8
VictorUmezuruike <victor.umezuruike@toyingroupsystems.com>
2023-01-20 10:53:05 +0100

    updated yaml file

--- infrastructure/modules/tf-state/tf-state.tf
+++ infrastructure/modules/tf-state/tf-state.tf
@@ -1,6 +1,6 @@
 #ecr repository
 resource "aws_ecr_repository" "production" {
-  name                 = "poc-env3"
+  name                 = "poc"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
